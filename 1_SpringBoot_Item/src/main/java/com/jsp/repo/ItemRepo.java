65a6783002f67c89bff12711f2dc69b84703a7dd
Pratik Raskar <140580307+pratikraskar45@users.noreply.github.com>
2026-07-19 01:21:26 +0530

    Delete 1_SpringBoot_Item directory

--- 1_SpringBoot_Item/src/main/java/com/jsp/repo/ItemRepo.java
+++
@@ -1,9 +0,0 @@
-package com.jsp.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.jsp.dto.Item;
-
-public interface ItemRepo extends JpaRepository<Item, Integer> {
-
-}
